c607ba96a77cf05497e9d509e0c6ec96fd364b8f
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-02-12 23:07:08 -0300

    Styles: Alter styles for default layout

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -2,6 +2,15 @@ import styled from "styled-components";
 
 export const LayoutContainer = styled.div`
     max-width: 90rem;
-    padding: 2rem 10rem;
+    padding: 0rem 10rem;
+    margin: auto auto;
+    
+
+    display: flex;
+    flex-direction: column;
+
+    /* justify-content: center; */
+    /* align-items: center; */
+    /* align-content: center; */
 
 `;
