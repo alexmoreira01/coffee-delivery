ff0ddd5136fe8dc077a2b43a50b7bff82fcf43e9
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-02-28 23:27:55 -0300

    Feature: rename variables form count total values cart

--- src/pages/Order/components/CoffeesSelected/components/CoffeesConfirmation/index.tsx
+++ src/pages/Order/components/CoffeesSelected/components/CoffeesConfirmation/index.tsx
@@ -6,25 +6,28 @@ import { CoffeesConfirmationTotal, ContainerCoffeesConfirmation } from "./styles
 
 export function CoffeesConfirmation() {
   const { totalPriceCart } = useContext(CoffeeCartContext);
+
+  const delivery = 3.5;
   
-  const formattedValue = FormartValue(totalPriceCart);
-  const totalWithSend = FormartValue(totalPriceCart + 3.5)
+  const formattedTotalValue = FormartValue(totalPriceCart);
+  const formattedTotalDelivery = FormartValue(delivery);
+  const formattedTotalWithDelivery = FormartValue(totalPriceCart + delivery)
 
   return (
     <ContainerCoffeesConfirmation>
       <div>
         <p>Total de itens</p>
-        <span>R$ {formattedValue}</span>
+        <span>R$ {formattedTotalValue}</span>
       </div>
 
       <div>
         <p>Entrega</p>
-        <span>R$ 3,50</span>
+        <span>R$ {formattedTotalDelivery}</span>
       </div>
 
       <CoffeesConfirmationTotal>
         <p>Total</p>
-        <span>R$ {totalWithSend}</span>
+        <span>R$ {formattedTotalWithDelivery}</span>
       </CoffeesConfirmationTotal>
 
       <Button text="Confirmar Pedido" />
